4fdace3ff0a14a1c929a028d006409151254e3b0
jrocia <69405400+jrocia@users.noreply.github.com>
2024-01-04 12:24:42 -0300

    Update appscansrc_create_config_scan_folder.ps1

--- scritps/appscansrc_create_config_scan_folder.ps1
+++ scritps/appscansrc_create_config_scan_folder.ps1
@@ -24,18 +24,18 @@ if ($compiledArtifactFolder -ne "none"){
   }
   write-output "login_file $aseHostname `"$aseToken`" -acceptssl" > script.scan
   write-output "RUNAS AUTO" >> script.scan
-  write-output "of `"$BITBUCKET_CLONE_DIR\$compiledArtifactFolder`"" >> script.scan
+  write-output "of `"$projectDir\$compiledArtifactFolder`"" >> script.scan
   write-output "sc `"$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt`" -scanconfig `"$scanConfig`" -name `"$CI_PROJECT_NAME-$CI_JOB_ID`"" >> script.scan
   write-output "report Findings pdf-detailed `"$CI_PROJECT_NAME-$CI_JOB_ID.pdf`" `"$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt`" -includeSrcBefore:5 -includeSrcAfter:5 -includeTrace:definitive -includeTrace:suspect -includeHowToFix" >> script.scan
   write-output "pa `"$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt`"" >> script.scan
   write-output "exit" >> script.scan
   
-  write-host "Config file created for compiled folder ($BITBUCKET_CLONE_DIR\$compiledArtifactFolder)."
+  write-host "Config file created for compiled folder ($projectDir\$compiledArtifactFolder)."
 }
 else{
   write-output "login_file $aseHostname `"$aseToken`" -acceptssl" > script.scan
   write-output "RUNAS AUTO" >> script.scan
-  write-output "of `"$BITBUCKET_CLONE_DIR`"" >> script.scan
+  write-output "of `"$projectDir`"" >> script.scan
   write-output "sc `"$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt`" -scanconfig `"$scanConfig`" -name `"$CI_PROJECT_NAME-$CI_JOB_ID`" -sourcecodeonly true" >> script.scan
   write-output "report Findings pdf-detailed `"$CI_PROJECT_NAME-$CI_JOB_ID.pdf`" `"$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt`" -includeSrcBefore:5 -includeSrcAfter:5 -includeTrace:definitive -includeTrace:suspect -includeHowToFix" >> script.scan
   write-output "pa `"$CI_PROJECT_NAME-$CI_JOB_ID.ozasmt`"" >> script.scan
